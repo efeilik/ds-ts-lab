9b054a5a6833c6b7901441476b8870c8671fb452
Efeİlik <efe.ilik@ug.bilkent.edu.tr>
2024-10-01 12:30:10 +0300

    Demo optional properties and parameters.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,51 +1,53 @@
-import {Friend, Colleague, ColleagueHistory } from './myTypes'
+import { Friend, Colleague, ColleagueHistory } from './myTypes'
 
-const friend1 : Friend = {
+const friend1: Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
     age: 25,
-  };
-  
-  const friend2 : Friend = {
+    dob: new Date("1998-11-20"),
+};
+
+const friend2: Friend = {
     name: "Jane Costello",
     phone: "086--12345",
     age: 31,
-  };
-  
-  export const friends : Friend[] = [friend1, friend2];
-  //console.log(friends[1]);
-  
-  //   -------------------
-  
-  const colleague1: Colleague = {
+    interests: ['Music', 'Sport']
+};
+
+export const friends: Friend[] = [friend1, friend2];
+//console.log(friends[1]);
+
+//   -------------------
+
+const colleague1: Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
-      email: "rgraham@company.com",
-      extension: 121,
+        email: "rgraham@company.com",
+        extension: 121,
     },
-  };
-  
-  const colleague2: Colleague  = {
+};
+
+const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
-      email: "pburke@company.com",
-      extension: 132,
+        email: "pburke@company.com",
+        extension: 132,
     },
-  };
-  
-  const colleague3: Colleague  = {
+};
+
+const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
-      email: "dos@company.com",
-      extension: 125,
+        email: "dos@company.com",
+        extension: 125,
     },
-  };
-  export const colleagues : ColleagueHistory = {
+};
+export const colleagues: ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
-  };
-  
+};
+
 //   console.log(colleagues.current[0]);
