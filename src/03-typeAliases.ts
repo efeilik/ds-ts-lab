de3522bb3f81b70ae66b065b7bf2636e3558488b
Efeİlik <efe.ilik@ug.bilkent.edu.tr>
2024-10-01 13:11:24 +0300

    Function with generics.

--- src/03-typeAliases.ts
+++ src/03-typeAliases.ts
@@ -4,10 +4,10 @@ import {
     Buddy,
     BuddyList,
     Administrator,
-  } from "./myTypes";
-  import { friends } from "./01-basics";
+} from "./myTypes";
+import { friends } from "./01-basics";
 
-  const colleague1: ColleagueV2 = {
+const colleague1: ColleagueV2 = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -38,56 +38,52 @@ function makeBuddyList(
     name: string,
     buddies: Buddy[],
     admin?: Administrator
-  ): BuddyList {
+): BuddyList {
     return {
-      name,
-      members: buddies,
-      administrator: admin,
+        name,
+        members: buddies,
+        administrator: admin,
     } as BuddyList;
     // The as operator above casts an object to a specific type.
-  }
-  // Tests for makeBuddyList
-  const myFootballBuddies = makeBuddyList(
+}
+// Tests for makeBuddyList
+const myFootballBuddies = makeBuddyList(
     "Football team",
     [colleague1, friends[0], colleague2],
     colleague1
-  )
-  
-  const myBandBuddies = makeBuddyList(
-      "Band name",
-      [colleague1, friends[1]]
-      // No administrator
-    )
-  
-  console.log(myFootballBuddies)
-  console.log(myBandBuddies)
-  //--------------------------------------
-  function findBuddyContact(list: BuddyList, name: string): string | undefined {
+)
+
+const myBandBuddies = makeBuddyList(
+    "Band name",
+    [colleague1, friends[1]]
+    // No administrator
+)
+
+console.log(myFootballBuddies)
+console.log(myBandBuddies)
+//--------------------------------------
+function findBuddyContact(list: BuddyList, name: string): string | undefined {
     for (const buddy of list.members) {
-      if (buddy.name === name) {
-        if ("phone" in buddy) {
-          return buddy.phone;
+        if (buddy.name === name) {
+            if ("phone" in buddy) {
+                return buddy.phone;
+            }
+            else {
+                return buddy.contact.email;
+            }
         }
-        else {
-          return buddy.contact.email;
-        }
-      }
-      return undefined;
+        return undefined;
     }
-  }
-  // Test for findBuddyContact.
-  console.log("Contact buddy at: ", findBuddyContact(myFootballBuddies, "Ralph Graham"));
+}
+// Test for findBuddyContact.
+console.log("Contact buddy at: ", findBuddyContact(myFootballBuddies, "Ralph Graham"));
 
-  function getBuddyListFriends(list: BuddyList): Friend[] {
+function getBuddyListFriends(list: BuddyList): Friend[] {
     return list.members.reduce<Friend[]>((friends, buddy) => {
-      // Eğer buddy bir Friend ise, diziye ekle
-      if ("phone" in buddy) {
-        friends.push(buddy);
-      }
-      return friends; // Akümülatörü döndür
-    }, []); // Boş bir dizi ile başla
-  }
+        if ("phone" in buddy) {
+            friends.push(buddy);
+        }
+        return friends;
+    }, []);
+}
 
-  const friendsInFootballBuddies = getBuddyListFriends(myFootballBuddies);
-  console.log("Friends in my football buddies:", friendsInFootballBuddies);
-  
