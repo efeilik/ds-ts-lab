71dd1b694684beafa120f7140ce6d47b0e0b94b2
Efeİlik <efe.ilik@ug.bilkent.edu.tr>
2024-10-01 12:37:43 +0300

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -71,3 +71,13 @@ function findFriends(friends: Friend[], criterion: (friend: Friend) => boolean):
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend: Friend, interest: string) {
+    if (!friend.interests) {
+        friend.interests = [];
+    }
+    friend.interests.push(interest);
+    return friend.interests;
+}
+
+console.log(addInterest(friends[1], 'Politics'));
